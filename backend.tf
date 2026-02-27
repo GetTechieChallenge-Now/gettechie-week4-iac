f4eff6e4fb935b6fa34cfb43b2104b17ff39975b
gettechiechallenge <gettechiechallenge@gmail.com>
2026-02-27 17:46:31 +0000

    added backend for state management

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "gettechie-week4-iac-statefile"
+    key    = "vpc/prod/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-state-locks-victory"
+    encrypt = true
+  }
+}
